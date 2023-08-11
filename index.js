610d7ecfff19c4c8e81fcac5ebd86baefa1360b1
Tristenh <tristenhobba1@gmail.com>
2023-08-11 15:28:53 +1000

    added start function with inquirer tableOfContents prompt , then if veiw all departments is selected, function veiwAllDepartments function is called to return the departments table

--- index.js
+++ index.js
@@ -6,3 +6,39 @@ const connection = mysql.createConnection({
   password: "12345678",
   database: "employeeTracker_db",
 });
+start();
+function start() {
+  inquirer
+    .prompt([
+      {
+        // select from list
+        type: "list",
+        name: "tableOfContents",
+        message: "Please select a option from the following list",
+        choices: [
+          "view all departments",
+          "view all roles",
+          "view all employees",
+          "add a department",
+          "add a role",
+          "add an employee",
+          "update an employee role",
+        ],
+      },
+    ])
+    .then((data) => {
+      if (data.tableOfContents.includes("view all departments")) {
+        viewAllDepartments();
+      }
+    });
+}
+
+function viewAllDepartments() {
+  connection.query(
+    "SELECT * FROM departments",
+    function (err, results, fields) {
+      console.table(results); // results contains rows returned by server
+      start();
+    }
+  );
+}
